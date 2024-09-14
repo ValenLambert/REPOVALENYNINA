dddda6442d1dea44b7ac6e034ed7d22d627b74bc
ninaellenberg <nellenberg@udesa.edu.ar>
2024-09-14 10:20:34 -0300

    pag NotFound (p.8) y navegacion

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from "react";
+import {Route, Switch} from 'react-router-dom'
 import Footer from "./components/Footer/Footer";
 import Nav from "./components/Nav/Nav";
 import Home from "./screens/Home";
+import Pelispopu from "./components/Pelispopu/Pelispopu"
+import Pelisencartel from "./components/Pelisencartel/Pelisencartel";
+import NotFound from './screens/NotFound'
+
+// import PelisFav from "./components/PelisFav/PelisFav" --> lo dejo listo para cuanod este el archivo de PelisFav
 
 
 function App() {
   return (
     <div>
-      <Nav/>
-      <Home/>
-      <Footer/>
+      {/* lo dejo listo el sistema de ruteo */}
+      <Nav />
+      <Switch>
+        <Route path="/" exact={true} component={Home} />
+        {/* <Route path="/favoritos" component={PelisFav} /> */}
+        <Route path="/vertodaspopu" component={Pelispopu} />
+        <Route path="/vertodascartelera" component={Pelisencartel} />
+        <Route path="" component={NotFound}/>
+      </Switch>
+      <Footer />
     </div>
+
   );
 }
 
---
+++ src/screens/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react-dom';
+
+function NotFound() {
+    return (
+        <>
+            <h1> ERROR: </h1>
+            <h1> Not Found 404</h1>
+        </>
+
+    )
+}
+export default NotFound;
