dda0dcad24e81054e9a08e6e6bf59d8c24c1da39
Valen Lambert <vlambert@udesa.edu.ar>
2024-09-23 08:58:22 -0300

    primera parte buscador

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,18 +1,56 @@
-import React from "react";
-import "./Form.css"
+import React, { Component } from "react";
+import "./Form.css";
+const apiKey = '42737f60c529bfe7e9586db8cb132a1c';
 
-function Form() {
-    return (
-        <React.Fragment>
-                <form >
-                    <input
-                        type="text"
-                        placeholder="Busca tu película..."
+class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            valor: "",
+            peliculas: [] 
+        };
+    }
+    componentDidMount() {
+        console.log("mount")
+        fetch(`https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1&api_key=${apiKey}`)
+            .then((resp) => resp.json())
+            .then((data) => {
+                this.setState({
+                    peliculas: data.results
+                    });
+            })
+            .catch((e) => console.log(e));
+    }
+
+
+    evitarSubmit(event) {
+        event.preventDefault(); 
+    }
+
+    filtrarPeliculas(nombrePelicula) {
+        const peliculasfiltradas = this.state.peliculas.filter(
+            (elem) => elem.title.toLowerCase().includes(nombrePelicula.toLowerCase()) 
+        );
+        console.log("buscando:", peliculasfiltradas);
+    }
+
+    controlarCambios(event) {
+        this.setState({ valor: event.target.value });
+        this.filtrarPeliculas(event.target.value);
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <form onSubmit={(event) => this.evitarSubmit(event)}>
+                    <input 
+                        onChange={(event) => this.controlarCambios(event)} 
+                        placeholder="Busca tu película..." 
                     />
-            </form>
-        </React.Fragment>
-    )
+                </form>
+            </React.Fragment>
+        );
+    }
 }
 
-
-export default Form
+export default Form;
